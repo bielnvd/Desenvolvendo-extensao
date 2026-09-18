0e91590be1a1697ce0fbb7f69cff26927d2ffe22
JOAO GABRIEL LOPES SANTOS <lbielsf6@gmail.com>
2026-09-18 22:39:11 +0000

    arquivos criados.

--- content.js
+++ content.js
@@ -1,1 +1,8 @@
+console.log("Content Script injetado com sucesso nesta página!"); 
 
+document.body.style.border = "4px solid #007bff"; chrome.runtime.sendMessage({ acao: "ping" }, 
+(response) => { 
+
+console.log("Resposta recebida nos bastidores:", response?.resposta); 
+
+});
---
+++ popup.js
@@ -0,0 +1,7 @@
+const act = document.querySelector('#btnAction'), stats = document.querySelector('#status')
+
+act.addEventListener('click', function() {
+
+stats.textContent = `Purple Rain in my limoneide!`
+
+})
